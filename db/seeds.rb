652e857468f3cd84875aebbb1666b6a479189bae
Cath Riddoch <cathriddoch@gmail.com>
2019-12-04 18:19:12 +0000

    before adding topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+topic1 = Topic.new({'topic' => 'dyslexia'})
+topic1.save
+topic2 = Topic.new({'topic' => 'dyspraxia'})
+topic2.save
+topic3 = Topic.new({'topic' => 'ADHD'})
+topic3.save
+topic3 = Topic.new({'topic' => 'sensory integration'})
+topic3.save
+topic4 = Topic.new({'topic' => 'autism'})
+topic4.save
